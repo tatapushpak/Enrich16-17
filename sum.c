d7b9a3a47a06ae31041eb677bc0269a290212293
tatapushpak <tatapushpak.ms@gmail.com>
2016-12-27 20:50:56 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,19 +1,12 @@
 #include<stdio.h>
-void main()
+int main()
 {
-  int n,i,s=0,c;
-  printf("enter the number:")';
-  scanf("%d",&n);
-  while(n!=0)
-  {
-   i=n%10;
-   s+=i;
-   n=n/10;
-   c++;
- }
- printf("number of digits present are:%d\n",c);
- printf("sum of digits is:%d",s);
- getch();
+  long long int n,i,s=0;
+  printf("enter the number:");
+  scanf("%lld",&n);
+  for(i=0;n>0;i++,n/=10)
+      s+=(n%10);
+ printf("number of digits  = %d\n",i);
+ printf("sum of digits     =%d",s);
+ return 0;
 }
-  
-   
