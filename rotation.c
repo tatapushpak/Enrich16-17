b96244a84b8dc42885986a6b508ba93d099fc3ac
tatapushpak <tatapushpak.ms@gmail.com>
2016-12-28 16:01:44 +0530

    Create rotation.c

---
+++ rotation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void) 
+{
+	int n,m;
+    scanf("%d %d",&n,&m);
+	int i=pow(10,5-m);
+    int sum=(n%i)*pow(10,m);
+    sum+=(n/pow(10,5-m));
+	printf("%d",sum);
+	return 0;
+}
