49e1ffbe02109c5f60f7c44d1e9503d71295c711
tatapushpak <tatapushpak.ms@gmail.com>
2016-12-28 18:44:36 +0530

    Update rotation.c

--- rotation.c
+++ rotation.c
@@ -4,10 +4,10 @@
 int main(void) 
 {
 	int n,m;
-    scanf("%d %d",&n,&m);
+	scanf("%d %d",&n,&m);
 	int i=pow(10,5-m);
-    int sum=(n%i)*pow(10,m);
-    sum+=(n/pow(10,5-m));
+	int sum=(n%i)*pow(10,m);
+	sum+=(n/pow(10,5-m));
 	printf("%d",sum);
 	return 0;
 }
