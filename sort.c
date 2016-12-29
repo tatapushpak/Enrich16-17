92985681e3cbb30ac2c4dbbc8257c530dadcdb6c
tatapushpak <tatapushpak.ms@gmail.com>
2016-12-29 18:47:48 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a,b[10],i,j;
+    printf("enter the number:");
+    scanf("%d",&a);
+    while(a>0)
+    {
+        p=a%10;
+        b[i]=p;
+        a=a/10;
+        i++;
+    }
+    for(j=0;j<=i;j++)
+    {
+        if(b[j]>b[j+1])
+        {
+            temp=b[j];
+            b[j]=b[j+1];
+            b[j+1]=temp;
+        }
+     }
+     a=0;
+    for(j=0;j<=i;j++)
+    {
+        a=(a*10)+b[j];
+    }
+    printf("the sorted number is:%d",a);
+    return 0;
+ }
+            
+    
