68407250dce16ecfd664ee2dbfd256c2e11e4662
tatapushpak <tatapushpak.ms@gmail.com>
2016-12-30 17:59:54 +0530

    Create palin.c

---
+++ palin.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  char a[100];
+  int l,flag=0,i=0;
+  scanf("%s",&a);
+  l=strlen(a);
+  for(i=0;i<l/2;i++)
+  {
+    if(a[i]!=a[(l-1)-i])
+    {
+      flag=1;
+      break;
+    }
+  }
+  if(flag==1)
+  {
+    printf("the string is an palindrome string");
+  }
+  else
+  {
+    printf("the string is an palindrome string");
+  }
+}
