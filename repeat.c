6e921f305d65405cd8d10d966a7b0dcd2e708a36
tatapushpak <tatapushpak.ms@gmail.com>
2016-12-31 13:35:04 +0530

    Create repeat.c

---
+++ repeat.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int a[20],i,num,n,c=0;
+	printf("enter the number of elements in an array:");
+	scanf("%d",&n);
+    printf("enter the number to be searched in an array:");
+	scanf("%d",&num);
+	printf("enter the array elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		if(a[i]==num)
+		{
+			c++;
+		}
+	}
+	printf("the given number is %d times repeated",c);
+} 
