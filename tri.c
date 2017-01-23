7d3d387176ab87a90c68d9378e39e015e3bd5940
tatapushpak <tatapushpak.ms@gmail.com>
2017-01-23 18:03:51 +0530

    Create tri.c

---
+++ tri.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+    int i,n;
+    for(i=1;i<=6;i++)
+    {
+        for(n=1;n<=i;n++)
+        {
+            if((n==1)||(n==i))
+            {
+                printf("1\t");
+            }
+            else
+            {
+                printf("%d\t",i);
+            }
+        }
+        printf("\n");
+    }
+}
