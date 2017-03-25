01eac3f7e1e466a8ae25054951e7eeb0ce3e38f4
tatapushpak <tatapushpak.ms@gmail.com>
2017-03-25 11:06:08 +0530

    Create spiralorder.c

---
+++ spiralorder.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+  int a[10][10],i,j,n;
+  int c=1;
+  printf("entert the matrix dim");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      if(i%2==0)
+      {
+        a[i][j]=c;
+        c++;
+      }
+      else
+      {
+        a[i][n-(j+1)]=c;
+        c++;
+      }   
+    }
+
+  }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("\t%d",a[i][j]);
+        }       
+        printf("\n");
+    }
+return 0;
+}
