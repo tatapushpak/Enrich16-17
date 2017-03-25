eee66e6ab274ccc15d2f56ac05c524925c458cbd
tatapushpak <tatapushpak.ms@gmail.com>
2017-03-25 11:38:30 +0530

    Create secondlargest.c

---
+++ secondlargest.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+	int n;
+	scanf("%d",&n);
+	int a[n],i,j;
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	  for(j=i+1;j<n;j++)
+	  {
+	      if(a[i]<a[j])
+	        {
+	            int temp;
+	            temp=a[i];
+	            a[i]=a[j];
+	            a[j]=temp;
+	        }
+	  }
+	  printf("%d",a[1]);
+	  return 0;
+}
